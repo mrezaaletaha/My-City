fe79c631a82e1db2b259dd68c5cf53df821d36aa
mrezaaletaha <mrezaaletaha@gmail.com>
2026-08-05 03:00:08 +0330

    remove reduntent comments

--- app/src/main/java/ir/miro/learning/mycity/data/Category.kt
+++ app/src/main/java/ir/miro/learning/mycity/data/Category.kt
@@ -2,7 +2,6 @@ package ir.miro.learning.mycity.data
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import ir.miro.learning.mycity.R
 
 /**
  * @author mrezaaletaha
@@ -13,10 +12,3 @@ class Category(
     @param:StringRes val title: Int = -1,
     @param:DrawableRes val img: Int = -1,
 )
-
-//enum class CategoryType(@StringRes title: Int, @DrawableRes image: Int) {
-//    COFFEE_SHOP(R.string.coffee_shops_title, R.drawable.coffee_shop),
-//    RESTAURANT(R.string.restaurants_title, R.drawable.restaurant),
-//    SHOPPING_CENTER(R.string.shopping_centers_title, R.drawable.shopping_center),
-//    ATTRACTIONS(R.string.attractions_title, R.drawable.attractions),
-//}
